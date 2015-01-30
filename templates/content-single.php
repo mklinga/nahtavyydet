0c8d6dcca88632b2072b6ab4501b992ee8daddc5
Markus Klinga <laite@gmx.com>
2015-01-30 15:49:13 +0200

    Add featured image to the posts

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,4 +1,12 @@
 <?php while (have_posts()) : the_post(); ?>
+    <header>
+<?php
+    if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+      the_post_thumbnail('large');
+}
+?>
+  <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header>
   <article <?php post_class(); ?>>
     <div class="entry-content">
       <?php the_content(); ?>
