554e9c485b9d7fd29139a854070c09981a036d4d
Markus Klinga <laite@gmx.com>
2015-01-31 11:16:35 +0200

    add some links to sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,2 +1,8 @@
-<h2>Nähtävyydet.fi</h2>
 <?php dynamic_sidebar('sidebar-primary'); ?>
+<div class="sidelinks">
+  <ul>
+    <li><a href="#">Erikoisuudet</a></li>
+    <li><a href="#">Klassikot</a></li>
+    <li><a href="#">Rentoutuminen</a></li>
+  </ul>
+</div>
