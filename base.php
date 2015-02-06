63e5bf04d5d4bbee3778d76945c3bca881e9ae7d
Markus Klinga <laite@gmx.com>
2015-02-06 16:02:56 +0200

    Add links under the post image

--- base.php
+++ base.php
@@ -14,6 +14,12 @@
 
   <div class="wrap container" role="document">
     <div class="content row">
+      <?php if (roots_display_sidebar()) : ?>
+        <aside class="sidebar" role="complementary">
+          <?php include roots_sidebar_path(); ?>
+        </aside><!-- /.sidebar -->
+      <?php endif; ?>
+
       <main class="main" role="main">
         <?php include roots_template_path(); ?>
       </main><!-- /.main -->
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,30 +1,33 @@
 <?php while (have_posts()) : the_post(); ?>
-    <header id="single-post-header">
+  <header id="single-post-header">
 <?php
-    if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-      the_post_thumbnail();
-}
+  if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+    the_post_thumbnail();
+  }
 ?>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-    </header>
+    <h1 class="entry-title"><?php the_title(); ?></h1>
 
-    <?php if (roots_display_sidebar()) : ?>
-      <aside class="sidebar" role="complementary">
-        <?php include roots_sidebar_path(); ?>
-      </aside><!-- /.sidebar -->
-    <?php endif; ?>
+<?php
+  $country = get_post_meta( get_the_ID(), 'attraction-country', true );
+  $city = get_post_meta( get_the_ID(), 'attraction-city', true );
+  $url = get_post_meta( get_the_ID(), 'attraction-homepage', true );
+?>
+    <a href="/<?php echo $country; ?>"><span class="subheader-country"><?php echo $country; ?></span></a> &gt; 
+    <a href="/<?php echo $city; ?>"><span class="subheader-city"><?php echo $city; ?></span></a>
+    <a target="_blank" href="<?php echo $url ?>"><span class="subheader-wikipedia">Wikipedia</span></a>
+  
+  </header>
+  <article <?php post_class(); ?>>
+    <div class="entry-content">
+      <?php the_content(); ?>
+    </div>
 
-    <article <?php post_class(); ?>>
-      <div class="entry-content">
-        <?php the_content(); ?>
-      </div>
+    <div class="attraction-list">
+      <?php get_template_part('templates/content', 'attraction'); ?>
+    </div>
 
-      <div class="attraction-list">
-        <?php get_template_part('templates/content', 'attraction'); ?>
-      </div>
-
-      <footer>
-        <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-      </footer>
-    </article>
+    <footer>
+      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+    </footer>
+  </article>
 <?php endwhile; ?>
