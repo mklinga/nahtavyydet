e9777b9ec431e1ccecf799717e1d5d96d39e24b1
Markus Klinga <markus.klinga@sc5.io>
2015-03-15 12:52:39 +0200

    Lay out the single post template, add the_title()

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,16 +1,19 @@
+<section id="single-post-featured-image">
 <?php
   if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-    the_post_thumbnail('medium');
+    the_post_thumbnail();
   }
 ?>
+</section>
 <div class="container">
   <main class="content row main" role="main">
 
     <aside class="sidebar" role="complementary">
-      Sidebar
+      &nbsp;
     </aside><!-- /.sidebar -->
 
     <article <?php post_class(); ?>>
+      <h1><?php the_title(); ?></h1>
       <div class="entry-content">
         <?php the_content(); ?>
       </div>
