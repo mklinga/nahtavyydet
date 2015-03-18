4ab39a0d1257fca2800df15f445efae2ca010c20
Markus Klinga <markus.klinga@sc5.io>
2015-03-18 15:37:03 +0200

    add home-main-content template

--- templates/content-home.php
+++ templates/content-home.php
@@ -2,38 +2,20 @@
   <div class="row">
 
     <div <?php post_class(); ?>>
-      <aside class="home-aside-left">
+      <aside class="hidden-xs hidden-sm home-aside-left">
         This is aside left
       </aside>
 
-      <div class="featured-post">
-        <?php get_template_part('templates/home', 'featured-article'); ?>
-      </div>
+      <section class="featured-post">
+          <?php get_template_part('templates/home', 'featured-article'); ?>
+      </section>
 
       <aside class="home-aside-right">
         <?php get_template_part('templates/home', 'aside-right'); ?>
       </aside>
 
-      <section>
-        <h2>Uusimmat kirjoitukset</h2>
-        <ul id="recent-posts">
-        <?php
-          $args = array( 
-            'numberposts' => '3',
-          );
-          $recent_posts = wp_get_recent_posts( $args );
-          foreach( $recent_posts as $recent ){
-  ?>
-          <li class="recent-post-item">
-            <a href="<?php echo get_permalink($recent["ID"]); ?>">
-              <?php echo get_the_post_thumbnail($recent["ID"], 'thumbnail'); ?>
-              <span class="post-title"><?php echo $recent["post_title"]; ?></span>
-            </a>
-          </li>
-  <?php
-          }
-        ?>
-        </ul>
+      <section class="articles">
+        <?php get_template_part('templates/home', 'main-content'); ?>
       </section>
     </div>
 
---
+++ templates/home-main-content.php
@@ -0,0 +1,19 @@
+<h2>Uusimmat kirjoitukset</h2>
+<ul id="recent-posts">
+<?php
+$args = array( 
+  'numberposts' => '5',
+);
+$recent_posts = wp_get_recent_posts( $args );
+foreach( $recent_posts as $recent ){
+?>
+  <li class="recent-post-item">
+    <a href="<?php echo get_permalink($recent["ID"]); ?>">
+      <?php echo get_the_post_thumbnail($recent["ID"], 'thumbnail'); ?>
+      <span class="post-title"><?php echo $recent["post_title"]; ?></span>
+    </a>
+  </li>
+<?php
+}
+?>
+</ul>
