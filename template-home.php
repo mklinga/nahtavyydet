8694c57021c1c56260462d5ede466504a50094f6
Markus Klinga <markus.klinga@sc5.io>
2015-03-18 13:10:30 +0200

    Add asides on both side to front page

--- template-home.php
+++ template-home.php
@@ -4,14 +4,6 @@ Template Name: Home Template
 */
 ?>
 
-<header class="container home-page-header">
-<img src="<?php bloginfo('template_directory'); ?>/assets/img/glasshouse.jpg">
-  <div id="home-header-text">
-    <h1>Nähtävyydet.fi</h1>
-    <h2>&lt;Iskulause tähän&gt;</h2>
-  </div>
-</header>
-
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/content', 'home'); ?>
 <?php endwhile; ?>
--- templates/content-home.php
+++ templates/content-home.php
@@ -1,33 +1,41 @@
-<div class="container">
-  <main class="content row main" role="main">
+<main class="container content main" role="main">
+  <div class="row">
 
-    <article <?php post_class(); ?>>
-      <div class="entry-content">
-        <?php the_content(); ?>
-      </div>
-    </article>
+    <div <?php post_class(); ?>>
+      <aside class="home-aside-left">
+        This is aside left
+      </aside>
+        <article>
+        <div class="entry-content">
+          <?php the_content(); ?>
+        </div>
+      </article>
+      <aside class="home-aside-right">
+        This is aside right
+      </aside>
 
-    <section>
-      <h2>Uusimmat kirjoitukset</h2>
-      <ul id="recent-posts">
-      <?php
-        $args = array( 
-          'numberposts' => '3',
-        );
-        $recent_posts = wp_get_recent_posts( $args );
-        foreach( $recent_posts as $recent ){
-?>
-        <li class="recent-post-item">
-          <a href="<?php echo get_permalink($recent["ID"]); ?>">
-            <?php echo get_the_post_thumbnail($recent["ID"], 'thumbnail'); ?>
-            <span class="post-title"><?php echo $recent["post_title"]; ?></span>
-          </a>
-        </li>
-<?php
-        }
-      ?>
-      </ul>
-    </section>
+      <section>
+        <h2>Uusimmat kirjoitukset</h2>
+        <ul id="recent-posts">
+        <?php
+          $args = array( 
+            'numberposts' => '3',
+          );
+          $recent_posts = wp_get_recent_posts( $args );
+          foreach( $recent_posts as $recent ){
+  ?>
+          <li class="recent-post-item">
+            <a href="<?php echo get_permalink($recent["ID"]); ?>">
+              <?php echo get_the_post_thumbnail($recent["ID"], 'thumbnail'); ?>
+              <span class="post-title"><?php echo $recent["post_title"]; ?></span>
+            </a>
+          </li>
+  <?php
+          }
+        ?>
+        </ul>
+      </section>
+    </div>
 
-  </main><!-- /.main -->
-</div><!-- /.content -->
+  </div><!-- /.content -->
+</main><!-- /.main -->
