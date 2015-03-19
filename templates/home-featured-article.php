9ed51800d1f2983c041292c36a04831eb5df2daf
Markus Klinga <markus.klinga@sc5.io>
2015-03-19 13:40:24 +0200

    One big featured article, two smaller ones on front page

--- templates/home-featured-article.php
+++ templates/home-featured-article.php
@@ -1,14 +1,25 @@
 <?php
-$query = new WP_Query('category_name=featured&posts_per_page=1');
+$query = new WP_Query(
+  array(
+    'category_name' => 'featured',
+    'posts_per_page' => 3,
+    'order_by' => 'date'
+  )
+);
 
 if ($query->have_posts() ) {
+  $num = 0;
   while ($query->have_posts() ) {
     $query->the_post();
+    $num++;
 ?>
-<div>
+  <div class="featured-item-<?php echo $num ?>">
   <a href="<?php echo the_permalink(); ?>">
 <?php
-    echo the_post_thumbnail();
+    if ($num == 1)
+      echo the_post_thumbnail();
+    else
+      echo the_post_thumbnail('medium');
 ?>
     <h2 class="featured-header"><?php the_title(); ?></h2>
 </a>
