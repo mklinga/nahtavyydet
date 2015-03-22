3c601a0d1e95d7a5cdca6915f6c4e5cd9287ccd2
Markus Klinga <markus.klinga@sc5.io>
2015-03-22 16:15:13 +0200

    Build deployment version locally and scp only the necessary

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# First we build theme
+grunt build
+
+# Make sure dist/ exists and is empty
+rm -rf ./dist
+mkdir -p ./dist
+
+# Copy necessary files to dist/ directory
+cp *.php dist/
+cp -r ./templates dist/
+cp -r ./lib dist/
+
+cp style.css dist/
+
+mkdir -p dist/assets
+cp ./assets/manifest.json dist/assets
+
+mkdir -p dist/assets/css
+cp assets/css/main.min.css dist/assets/css
+
+mkdir -p dist/assets/js
+cp assets/js/scripts.min.js dist/assets/js
+
+mkdir -p dist/assets/js/vendor/
+cp ./assets/js/vendor/modernizr.min.js ./dist/assets/js/vendor/modernizr.min.js
+
+mkdir -p dist/assets/fonts
+cp ./assets/fonts/* ./dist/assets/fonts
+
+# Make a package
+tar cjf dist.tar.bz2 dist
+
+# Copy to the server
+scp -r dist.tar.bz2 laite@suspicious.website:/nahtavyydet
+
+# Do the server-side dance
+ansible-playbook -K deploy.yml
+
+# Remove the package and directory
+rm dist.tar.bz2
+rm -r ./dist
