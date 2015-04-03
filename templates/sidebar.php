b126212a34ab9cc7b6f1c1029e35c3518b6aa9fc
Markus Klinga <markus.klinga@sc5.io>
2015-04-03 12:48:51 +0300

    Empty the sidebar for now

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,18 +1,20 @@
 <?php
 // dynamic_sidebar('sidebar-primary');
 ?>
-<h1>Navigointi</h1>
-
-<nav class="sidebar-nav">
-  <ul>
-    <li class="selected">
-      <span><?php echo $country; ?></span>
-      <ul>
-        <li class="selected">Tampere</li>
-        <li>Helsinki</li>
-      </ul>
-    </li>
-    <li class="">Ruotsi</li>
-    <li class="">Kroatia</li>
-  </ul>
-</nav>
+<!--
+  <h1>Navigointi</h1>
+  
+  <nav class="sidebar-nav">
+    <ul>
+      <li class="selected">
+        <span><?php echo $country; ?></span>
+        <ul>
+          <li class="selected">Tampere</li>
+          <li>Helsinki</li>
+        </ul>
+      </li>
+      <li class="">Ruotsi</li>
+      <li class="">Kroatia</li>
+    </ul>
+  </nav>
+-->
