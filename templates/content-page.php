e4638d717cc356cbb11071ebce80f1e8dacf4c80
Markus Klinga <markus.klinga@sc5.io>
2015-04-03 12:43:51 +0300

    further generalize theme

--- templates/content-page.php
+++ templates/content-page.php
@@ -4,41 +4,50 @@
 
   <?php get_template_part('templates/article', 'header'); ?>
 
+  <?php
+    $country = get_post_meta( get_the_ID(), 'location-country', true );
+    $city = get_post_meta( get_the_ID(), 'location-city', true );
+    $pic = get_post_meta( get_the_ID(), 'location-picture-credit', true );
+  ?>
 <?php
-  $country = get_post_meta( get_the_ID(), 'location-country', true );
-  $city = get_post_meta( get_the_ID(), 'location-city', true );
-  $pic = get_post_meta( get_the_ID(), 'location-picture-credit', true );
+
+    /*
+     * TODO: Make top pages available!
+     */
 ?>
-    <a href="/<?php echo $country; ?>"><span class="subheader-country"><?php echo $country; ?></span></a> &gt; 
-    <a href="/<?php echo $city; ?>"><span class="subheader-city"><?php echo $city; ?></span></a>
-    <span class="subheader-credit"><?php echo $pic; ?></span>
+  <!-- <a href="/<?php echo $country; ?>"> -->
+    <span class="subheader-country"><?php echo $country; ?></span>
+  <!-- </a> -->
+  &gt; 
+  <a href="/<?php echo $city; ?>"><span class="subheader-city"><?php echo $city; ?></span></a>
+  <span class="subheader-credit"><?php echo $pic; ?></span>
   
-  </section>
-    <div class="article-content container">
-      <main class="content row main" role="main">
-    <?php if (roots_display_sidebar()) : ?>
-      <aside class="sidebar" role="complementary">
-        <?php include roots_sidebar_path(); ?>
-      </aside><!-- /.sidebar -->
-    <?php endif; ?>
+</section>
+  <div class="article-content container">
+    <main class="content row main" role="main">
+  <?php if (roots_display_sidebar()) : ?>
+    <aside class="sidebar" role="complementary">
+      <?php include roots_sidebar_path(); ?>
+    </aside><!-- /.sidebar -->
+  <?php endif; ?>
 
-  <article <?php post_class(); ?>>
-    <div class="entry-content">
-      <?php the_content(); ?>
-    </div>
+<article <?php post_class(); ?>>
+  <div class="entry-content">
+    <?php the_content(); ?>
+  </div>
 
-    <div class="attraction-list">
-      <?php get_template_part('templates/content', 'attraction'); ?>
-    </div>
+  <div class="attraction-list">
+    <?php get_template_part('templates/content', 'attraction'); ?>
+  </div>
 
-    <div class="related-posts">
-      <?php get_template_part('templates/content', 'related'); ?>
-    </div>
+  <div class="related-posts">
+    <?php get_template_part('templates/content', 'related'); ?>
+  </div>
 
-    <footer>
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-    </footer>
-  </article>
-      </main><!-- /.main -->
-    </div><!-- /.content -->
+  <footer>
+    <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+  </footer>
+</article>
+    </main><!-- /.main -->
+  </div><!-- /.content -->
 <?php endwhile; ?>
