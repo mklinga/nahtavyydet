b52d4b0800e8ea503070e028eeac82ccc6d78e0c
Markus Klinga <markus.klinga@sc5.io>
2015-04-05 12:49:57 +0300

    Moving to skeleton, part 2

--- templates/content-page.php
+++ templates/content-page.php
@@ -20,34 +20,33 @@
   <!-- </a> -->
   &gt; 
   <a href="/<?php echo $city; ?>"><span class="subheader-city"><?php echo $city; ?></span></a>
-  <span class="subheader-credit"><?php echo $pic; ?></span>
+  <span class="post-caption"><?php echo $pic; ?></span>
   
 </section>
   <div class="article-content container">
     <main class="content row main" role="main">
-  <?php if (roots_display_sidebar()) : ?>
-    <aside class="sidebar" role="complementary">
-      <?php include roots_sidebar_path(); ?>
-    </aside><!-- /.sidebar -->
-  <?php endif; ?>
 
-<article <?php post_class(); ?>>
-  <div class="entry-content">
-    <?php the_content(); ?>
-  </div>
+      <aside class="four columns sidebar" role="complementary">
+        &nbsp;
+      </aside><!-- /.sidebar -->
 
-  <div class="attraction-list">
-    <?php get_template_part('templates/content', 'attraction'); ?>
-  </div>
+      <article <?php post_class('eight columns'); ?>>
+        <div class="entry-content">
+          <?php the_content(); ?>
+        </div>
 
-  <div class="related-posts">
-    <?php get_template_part('templates/content', 'related'); ?>
-  </div>
+        <div class="attraction-list">
+          <?php get_template_part('templates/content', 'attraction'); ?>
+        </div>
 
-  <footer>
-    <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-  </footer>
-</article>
+        <div class="related-posts">
+          <?php get_template_part('templates/content', 'related'); ?>
+        </div>
+
+        <footer>
+          <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+        </footer>
+      </article>
     </main><!-- /.main -->
   </div><!-- /.content -->
 <?php endwhile; ?>
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,18 +1,16 @@
-
  
 <section class="article-featured-image" id="single-post-featured-image">
-<?php get_template_part('templates/article', 'header'); ?>
+  <?php get_template_part('templates/article', 'header'); ?>
 </section>
-
 
 <div class="article-content container">
   <main class="content row main" role="main">
 
-    <aside class="sidebar" role="complementary">
+    <aside class="four columns sidebar" role="complementary">
       &nbsp;
     </aside><!-- /.sidebar -->
 
-    <article <?php post_class(); ?>>
+    <article <?php post_class('eight columns'); ?>>
       <div class="entry-content">
         <?php the_content(); ?>
       </div>
