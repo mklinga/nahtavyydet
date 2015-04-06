56345c5ab0d045340f13b52d682b24137978c563
Markus Klinga <markus.klinga@sc5.io>
2015-04-06 16:57:08 +0300

    Fix article header on mobile screens

--- templates/article-header.php
+++ templates/article-header.php
@@ -4,12 +4,12 @@
   }
 ?>
 
-  <div class="image-title-box">
-    <h1><?php the_title(); ?></h1>
-    <h2><?php echo get_the_date(); ?></h2>
-  </div>
 <?php
   $caption = get_post(get_post_thumbnail_id())->post_excerpt;
   if ($caption != '')
     echo '<span class="post-caption">' . $caption . '</span>';
 ?>
+  <div class="image-title-box">
+    <h1><?php the_title(); ?></h1>
+    <h2><?php echo get_the_date(); ?></h2>
+  </div>
