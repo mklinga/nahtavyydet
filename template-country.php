3b2b37986e1341508924f29c6f57fb4c39809613
Markus Klinga <markus.klinga@sc5.io>
2015-04-09 12:22:26 +0300

    remove unnecessary template files

--- template-country.php
+++ template-country.php
@@ -3,4 +3,28 @@
 Template Name: Country Template
 */
 
-get_template_part('templates/collection', 'country');
+while (have_posts()) : the_post(); ?>
+
+<div class="row">
+  <section class="article-featured-image" id="country-featured-image">
+    <?php get_template_part('templates/article', 'header'); ?>
+  </section>
+</div>
+
+<main class="container content main" role="main">
+
+  <div class="row">
+    <section class="twelve columns articles">
+      <?php the_content(); ?>
+    </section>
+  </div>
+
+<?php endwhile; ?>
+
+  <div>
+    <?php
+      get_template_part('templates/country', 'content');
+    ?>
+  </div>
+</main><!-- /.main -->
+
