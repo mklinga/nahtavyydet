c075f9d99b41331a789a90e22e2d7d3fe17308b7
Markus Klinga <markus.klinga@sc5.io>
2015-04-13 14:29:42 +0300

    remove content-home.php

--- template-home.php
+++ template-home.php
@@ -5,5 +5,15 @@ Template Name: Home Template
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'home'); ?>
+<main class="container content main" role="main">
+
+    <?php get_template_part('templates/home', 'featured-article'); ?>
+
+  <div class="row">
+    <section class="twelve columns articles">
+      <?php get_template_part('templates/home', 'main-content'); ?>
+    </section>
+  </div>
+
+</main><!-- /.main -->
 <?php endwhile; ?>
