5d6376f493ed040506223d792a78a5542264fb0a
Markus Klinga <markus.klinga@sc5.io>
2015-05-02 11:30:50 +0300

    Add frontpage google ad

--- templates/home-aside-right.php
+++ templates/home-aside-right.php
@@ -2,6 +2,16 @@
 // if ( function_exists('dynamic_sidebar')) {
 //   dynamic_sidebar('frontpage-right');
 // }
-//
 ?>
-<div class="google-large-rectangle"></div>
+<div class="google-large-rectangle">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- nahtavyydet-etusivu -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-7611097420441137"
+     data-ad-slot="1868643202"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
