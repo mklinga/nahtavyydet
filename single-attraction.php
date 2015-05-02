7ae078a98ea79b0a3a428f2d8a1e9ae20c9dbca5
Markus Klinga <markus.klinga@sc5.io>
2015-05-02 11:04:10 +0300

    Unify page layout for attractions

--- single-attraction.php
+++ single-attraction.php
@@ -1,26 +1,27 @@
 <?php while (have_posts()) : the_post(); ?>
-<div class="article-content container">
 
-  <div class="row">
+<div class="article-content container">
+  <main class="content row main" role="main">
+
+    <article <?php post_class('eight columns'); ?>>
+
       <section class="article-featured-image" id="single-attraction-featured-image">
         <?php get_template_part('templates/article', 'header'); ?>
       </section>
-  </div>
 
-  <main class="content row main" role="main">
-    <aside class="four columns sidebar" role="complementary">
-      &nbsp;
-    </aside><!-- /.sidebar -->
-
-    <article <?php post_class('eight columns'); ?>>
-      <div class="entry-content">
+      <section class="entry-content">
         <?php the_content(); ?>
-      </div>
+      </section>
 
       <section class="related-articles">
         <?php get_template_part('templates/attraction', 'related'); ?>
       </section>
+
     </article>
+
+    <aside class="four columns sidebar" role="complementary">
+      &nbsp;
+    </aside><!-- /.sidebar -->
 
   </main>
 </div>
