bdcc2f33483c9c2644e9501a90f8174d94d0ab3f
Markus Klinga <laite@gmx.com>
2015-06-27 11:06:59 +0300

    Get rid of ugly title-box on articles

--- templates/article-header.php
+++ templates/article-header.php
@@ -1,29 +1,15 @@
+  <div class="image-title-box">
+    <section class="article-title">
+      <h1><?php the_title(); ?></h1>
+      <h2> <?php echo get_the_date('F Y'); ?> </h2>
+    </section>
 <?php
   if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
     the_post_thumbnail('extra-large');
   }
-?>
 
-<?php
   $caption = get_post(get_post_thumbnail_id())->post_excerpt;
   if ($caption != '')
     echo '<span class="post-caption">' . $caption . '</span>';
 ?>
-  <div class="image-title-box">
-    <h1><?php the_title(); ?></h1>
-    <h2>
-<?php
-    $post_type = get_post_meta(get_the_ID(), 'location-type', true);
-    if ($post_type == "Valtio")
-      echo get_post_meta(get_the_ID(), 'location-continent', true);
-    else if ($post_type == "Kaupunki")
-      echo get_post_meta(get_the_ID(), 'location-country', true);
-    else if (get_post_meta(get_the_ID(), 'attraction-city') != "")
-      echo get_post_meta(get_the_ID(), 'attraction-city', true);
-    else if (($post_type == "Muu") || ($post_type == "Maanosa"))
-      echo "";
-    else
-      echo the_modified_date('F Y');
-?>
-    </h2>
   </div>
